40deceb86f66262aa845c4fc8dddb77e8ac26d55
Cesar Velásquez Cordova <ces.velasquez@hotmail.com>
2021-04-03 17:41:28 -0600

    header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,38 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      white: colors.white,
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      green: colors.green,
+      orange: colors.orange
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'secondary': '#353535',
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   variants: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
+  }
 }
