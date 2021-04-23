a74c55b2187bc962a6429ae06364832404636ebc
Cesar Velásquez Cordova <ces.velasquez@hotmail.com>
2021-04-23 15:34:15 -0600

    Course ended

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    purgecss({
+      content: ['./**/*.html'],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }), // Libreria que omite todas las clases y/o instrucciones css que no se utilizan
+    require('cssnano')({
+      preset: 'default',
+    }), // Libreria que minifica el archivo css para tener menor peso en un ambiente de produccion
+  ],
 };
